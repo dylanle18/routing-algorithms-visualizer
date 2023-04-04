cb59803bc1f061cb644d413a5f520566eec366a8
Nabil Mansour <nabilmansour1999@gmail.com>
2023-04-04 18:11:58 -0400

    Added DVR and implemented the UI for the app

--- main.py
+++ main.py
@@ -1,83 +1,172 @@
 import streamlit as st
 import graphviz as gv
+import pandas as pd
+from utils import Edge, Graph
 
-# TODO: Import final functions here
-from mock import mock
+def render_settings(graph : Graph):
+    st.title('Visualizer Settings:')
 
-st.title('Routing Algorithms Visualizer')
+    # get nodes for selection
+    nodes = graph.nodes
 
-# edges init
-edges = [
-    {'node1': 'A', 'node2': 'B', 'isPath': 0, 'cost': 100},
-    {'node1': 'A', 'node2': 'C', 'isPath': 0, 'cost': 200},
-    {'node1': 'B', 'node2': 'C', 'isPath': 0, 'cost': 300},
-    {'node1': 'B', 'node2': 'D', 'isPath': 0, 'cost': 400},
-    {'node1': 'C', 'node2': 'D', 'isPath': 0, 'cost': 500},
-    {'node1': 'C', 'node2': 'D', 'isPath': 0, 'cost': 600},
-    {'node1': 'C', 'node2': 'F', 'isPath': 0, 'cost': 700},
-    {'node1': 'E', 'node2': 'F', 'isPath': 0, 'cost': 800}
-]
+    if graph.is_empty:
+        st.warning("Empty Graph")
+        return None,None,None,None
 
-# get nodes for selection
-nodes = []
-for edge in edges:
-    if edge['node1'] not in nodes:
-        nodes.append(edge['node1'])
-    if edge['node2'] not in nodes:
-        nodes.append(edge['node2'])
+    # select start and end nodes
+    start = st.selectbox("Select start node :red_circle:", ['Select Node']+nodes, 0)
+    end = st.selectbox("Select end node :large_blue_circle:", ['Select Node']+nodes, 0)
 
-# select start and end nodes
-start = st.multiselect("Select start node", nodes, [], max_selections=1)
-end = st.multiselect("Select end node", nodes, [], max_selections=1)
+    # check if start and end are not the same
+    if start == end and start != 'Select Node':
+        st.warning("Please select different start and end nodes")
+        return None,None,None,None
 
-# check if start and end are not the same
-if start == end:
-    st.warning("Please select different start and end nodes")
+    s = None
+    e = None
 
-# final render
-elif start != [] and end != []:
-    col1, col2 = st.columns(2)
+    if start != 'Select Node':
+        s = start
 
-    # TODO: use Dijkstra instead of mock
-    edges = mock(edges)
+    if end != 'Select Node':
+        e = end
 
+    algo = st.selectbox("Select Algorithm to run", ["Select Algorithm", "Dijkstra's", "DVR"], 0)
+    if algo == "Select Algorithm":
+        return None, None, s, e
+
+    if algo == 'DVR':
+        cost, path = graph.get_shortest_path_DV(start_node=start[0], end_node=end[0])
+
+    if algo == "Dijkstra's":
+        # TODO: use Dijkstra
+        cost, path = -999, ['ALG NOT ADDED']
+
+    return cost, path, s, e
+
+
+
+def render_customization():
+    default_graph = {
+        'N1' : ['A','A','B','B','C','C','E'],
+        'N2' : ['B','C','C','D','D','F','F'],
+        'Cost' : [100.0,200.0,300.0,400.0,600.0,700.0,800.0]
+    }
+
+    st.subheader('Set Graph Values')
+
+    col1, col2 = st.columns([1, 2])
     with col1:
-        st.title("Q1: Dijkstra's")
-        graph = gv.Graph()
-        graph.attr('node', shape='square')
-        graph.node(start[0])
-        graph.node(end[0])
-        graph.attr('node', shape='circle')
+        if st.button('Set Graph to Default Example'):
+            graph_values = pd.DataFrame(default_graph)
 
-        for edge in edges:
-            if edge['isPath'] > 0:
-                dir = "forward" if edge['isPath'] == 1 else "back"
-                graph.edge(edge['node1'], edge['node2'], label=str(
-                    edge['cost']), color='red', dir=dir)
-            else:
-                graph.edge(edge['node1'], edge['node2'],
-                           label=str(edge['cost']))
-
-        st.graphviz_chart(graph, use_container_width=True)
-
-    # TODO: use DV instead of mock
-    edges = mock(edges)
+    graph_values = pd.DataFrame(default_graph)
 
     with col2:
-        st.title("Q2: DV")
-        graph = gv.Graph()
-        graph.attr('node', shape='square')
-        graph.node(start[0])
-        graph.node(end[0])
-        graph.attr('node', shape='circle')
+        if st.button('Reset Graph'):
+            graph_values = pd.DataFrame({
+                'N1' : pd.Series(dtype='str'),
+                'N2' : pd.Series(dtype='str'),
+                'Cost' : pd.Series(dtype='float')
+            })
 
-        for edge in edges:
-            if edge['isPath'] > 0:
-                dir = "forward" if edge['isPath'] == 1 else "back"
-                graph.edge(edge['node1'], edge['node2'], label=str(
-                    edge['cost']), color='blue', dir=dir)
-            else:
-                graph.edge(edge['node1'], edge['node2'],
-                           label=str(edge['cost']))
+    graph_values = graph_values.reset_index(drop=True)
+    graph_values = st.experimental_data_editor(graph_values, num_rows="dynamic", use_container_width=True)
+    graph_values = graph_values.replace('', float('NaN'))
+    graph_values.dropna(how='any', inplace=True)
 
-        st.graphviz_chart(graph, use_container_width=True)
+    st.text("Note that you can delete a row by selecting it and pressing 'DELETE'")
+
+    return graph_values
+
+
+# Main app
+st.title('Routing Algorithms Visualizer')
+
+graph_values = render_customization()
+
+graph = Graph()
+graph.create_graph_from_df(graph_values)
+
+cost, path_list, s_node, e_node = None, None, None, None
+
+if graph.is_empty:
+    st.warning("Your Graph is empty. Please fill it first")
+
+with st.sidebar:
+    cost, path_list, s_node, e_node = render_settings(graph)
+
+graph_visual = gv.Graph()
+graph_visual.attr(rankdir='LR')
+
+graph_visual.attr('node', width='.7', height='.7', style='filled')
+
+if s_node:
+    graph_visual.attr('node', color='darksalmon')
+    graph_visual.node(s_node)
+
+if e_node:
+    graph_visual.attr('node', color='lightblue')
+    graph_visual.node(e_node)
+
+st.subheader('The Resultant Graph')
+
+
+
+# Back and Next Buttons
+st.text('Press Back and Next to go through the path')
+if 'step_number' in st.session_state:
+    if path_list:
+        step_number = min(st.session_state['step_number'], len(path_list))
+    else:
+        step_number = st.session_state['step_number']
+else:
+    step_number = 1
+col1, col2, col3 = st.columns([.5,.5, 3])
+with col1:
+    if st.button('Back', disabled=not path_list):
+        step_number = max(step_number-1, 1)
+with col2:
+    if st.button('Next', disabled=not path_list):
+        step_number = min(step_number+1, len(path_list))
+with col3:
+    if path_list:
+        st.write(f'Step {step_number-1}')
+    else:
+        st.write('Step ...')
+st.session_state['step_number'] = step_number
+
+
+
+# Generate path digraph
+path = {}
+if path_list:
+    for i in range(len(path_list[:step_number])-1):
+        path[path_list[i]] = path_list[i+1]
+
+
+
+# Render graph
+if not graph.is_empty:
+    graph_visual.attr('node', color='gray')
+    graph_visual.attr('node', shape='oval')
+    for edge in graph.edges:
+        if edge.n1 in path and path[edge.n1] == edge.n2:
+            graph_visual.edge(edge.n1, edge.n2, label=str(edge.cost), dir='forward', color='darkseagreen4', penwidth='2', arrowsize='1.5')
+        elif edge.n2 in path and path[edge.n2] == edge.n1:
+            graph_visual.edge(edge.n1, edge.n2, label=str(edge.cost), dir='back', color='darkseagreen4', penwidth='2', arrowsize='1.5')
+        else:
+            graph_visual.edge(edge.n1, edge.n2, label=str(edge.cost))
+    st.graphviz_chart(graph_visual)
+
+
+
+# Render cost and path
+if path_list:
+    st.write(f'Cost is `{cost}`'.format(cost))
+    path_list_str = '['
+    for p in path_list:
+        path_list_str += p+'->'
+    path_list_str = path_list_str[:-2]
+    path_list_str += ']'
+    st.write(f'Path is `{path_list_str}`'.format(path_list_str))
---
+++ utils.py
@@ -0,0 +1,114 @@
+import pandas as pd
+
+class Edge:
+    def __init__(self, n1 : str, n2 : str, cost : float):
+        self.n1 = n1
+        self.n2 = n2
+        self.cost = cost
+
+    def __str__(self) -> str:
+        return 'Edge('+self.n1+','+self.n2+','+str(self.cost)+')'
+
+class Graph:
+    def __init__(self, edges : list[Edge] = []):
+        self.edges : list[Edge] = edges
+        self.nodes : list[str] = self._get_nodes()
+        self.is_empty : bool = edges == []
+
+    def create_graph_from_df(self, df : pd.DataFrame):
+        self.edges = []
+        for _, row in df.iterrows():
+            edge = Edge(row['N1'], row['N2'], row['Cost'])
+            self.edges.append(edge)
+        self.nodes = self._get_nodes()
+        self.is_empty : bool = self.edges == []
+
+    def __str__(self) -> str:
+        to_return = 'Graph(\n'
+        for edge in self.edges:
+            to_return += '    '+ str(edge) + '\n'
+        to_return += ')'
+        return to_return
+
+    def _get_nodes(self) -> list[str]:
+        nodes : list[str] = []
+        for edge in self.edges:
+            if edge.n1 not in nodes:
+                nodes.append(edge.n1)
+            if edge.n2 not in nodes:
+                nodes.append(edge.n2)
+        return nodes
+    
+    def get_cost(self, n1 : str, n2 : str):
+        for edge in self.edges:
+            if (edge.n1 == n1 and edge.n2 == n2) or (edge.n1 == n2 and edge.n2 == n1):
+                return edge.cost
+        return None
+
+    def get_immediate_neighbor_nodes(self, node : str):
+        immediate_neighbors = []
+        for edge in self.edges:
+            if edge.n1 == node:
+                immediate_neighbors.append(edge.n2)
+            elif edge.n2 == node:
+                immediate_neighbors.append(edge.n1)
+        return immediate_neighbors
+
+    def get_shortest_path_DV(self, start_node: str, end_node: str):
+        # Initialize distance vector for each node
+        distance_vectors: dict[str, dict[str, float]] = {}
+        for node in self.nodes:
+            distance_vectors[node] = {}
+            for neighbor in self.nodes:
+                if node == neighbor:
+                    distance_vectors[node][neighbor] = 0
+                else:
+                    distance_vectors[node][neighbor] = float('inf')
+
+        # Set distance vector for edges in the graph
+        for edge in self.edges:
+            distance_vectors[edge.n1][edge.n2] = edge.cost
+            distance_vectors[edge.n2][edge.n1] = edge.cost
+
+        # Apply distance vector graph algorithm
+        for k in self.nodes:
+            for i in self.nodes:
+                for j in self.nodes:
+                    if distance_vectors[i][j] > distance_vectors[i][k] + distance_vectors[k][j]:
+                        distance_vectors[i][j] = distance_vectors[i][k] + distance_vectors[k][j]
+
+        # Extract shortest path from distance vectors
+        shortest_path = [start_node]
+        current_node = start_node
+
+        while current_node != end_node:
+            # Get the immediate neighbors of the current node
+            immediate_neighbors = self.get_immediate_neighbor_nodes(current_node)
+            cost_of_end_node_from_immediate_neighbors = {n : distance_vectors[n][end_node] for n in immediate_neighbors}
+
+            # Find next node
+            potential_new_current_node = current_node
+            for n in immediate_neighbors:
+
+                # If at the end node, check if it's the final hop
+                if n == end_node:
+                    if distance_vectors[current_node][n] == self.get_cost(current_node, n):
+                        potential_new_current_node = n 
+
+                # Otherwise, hop to the node with the least cost
+                elif cost_of_end_node_from_immediate_neighbors[n] + distance_vectors[current_node][n] <= distance_vectors[current_node][end_node]:
+                    potential_new_current_node = n
+
+            # Update the current node
+            current_node = potential_new_current_node
+
+            # Update shortest path
+            shortest_path.append(current_node)
+
+            # In case of infinite loop
+            if len(shortest_path) > len(self.nodes): break
+
+        # Get total cost from distance vector
+        total_cost = distance_vectors[start_node][end_node]
+
+        return total_cost, shortest_path
